2eb3737dd296b9aa9e73367df0f9a1de76497dc2
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-06 14:05:01 +0200

    Created Employee model and modified the ApplicationUser model.

--- GoogleAuth/Server/Models/ApplicationUser.cs
+++ GoogleAuth/Server/Models/ApplicationUser.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using GoogleAuth.Shared.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace GoogleAuth.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
---
+++ GoogleAuth/Shared/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GoogleAuth.Shared.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        [JsonIgnore]
+        public string FullName { get => FirstName + " " + LastName; }
+        public string Place { get; set; } = string.Empty;
+    }
+}
