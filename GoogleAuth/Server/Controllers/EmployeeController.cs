faf565ac451fc439d21b39fdaef2e8fc69d64a11
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-06 14:18:53 +0200

    Created EmployeeController and recieved the user's employees on the index page.

---
+++ GoogleAuth/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using GoogleAuth.Server.Data;
+using GoogleAuth.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace GoogleAuth.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployees()
+        {
+            var user = await _context.Users
+                .Include(u => u.Employees)
+                .FirstOrDefaultAsync(u => u.Id == User
+                .FindFirstValue(ClaimTypes.NameIdentifier));
+
+            if (user is null)
+                return NotFound();
+            return Ok(user.Employees);
+        }
+    }
+}
